784c7c89b8e18934673f334c9032d41e91a7e0d6
Radu Oprita <oprita@gmail.com>
2024-07-03 03:18:15 +0200

    more filtering

--- app/jobs/reprocess_psus_job.rb
+++ app/jobs/reprocess_psus_job.rb
@@ -4,6 +4,53 @@ class ReprocessPsusJob < ApplicationJob
   # TODO add logging
 
   CYBENETICS_PSU_URL = "https://www.cybenetics.com/index.php?option=power-supplies"
+  SKIPPED_BRANDS =
+    [
+      '1ST PLAYER',
+      'ABYSM GAMING',
+      'AEROCOOL',
+      'APEXGAMING',
+      'AQIRYS',
+      'ARESGAME',
+      'BITFENIX',
+      'CASECOM',
+      'CHANNEL WELL TECHNOLOGY',
+      'CHIEFTEC',
+      'CHIEFTRONIC',
+      'COUGAR',
+      'FRACTAL DESIGN',
+      'GALAX',
+      'GAME G FACTOR',
+      'GAMEMAX',
+      'GIGABYTE',
+      'HIGH POWER',
+      'INWIN',
+      'KBM! GAMING',
+      'KINPOWER',
+      'KOLINK',
+      'LIAN LI',
+      'MAXPOWER',
+      'MICRONICS',
+      'MISTEL',
+      'MSI',
+      'PC POWER & COOLING',
+      'PCCOOLER',
+      'PCYES',
+      'PICHAU GAMING',
+      'POWERSPEC',
+      'RIOTORO',
+      'ROSEWILL',
+      'SALCOMP',
+      'SAMA',
+      'SEGOTEP',
+      'SHARKOON',
+      'SUPERFRAME',
+      'VETROO',
+      'WENTAI',
+      'XPG',
+      'ZALMAN',
+      'ZHONG YUAN POWER',
+    ]
 
   PSU     = Struct.new(
     *PowerSupply.column_names.map(&:to_sym) - [:id, :created_at, :updated_at]
@@ -11,8 +58,8 @@ class ReprocessPsusJob < ApplicationJob
   PSUS    = {}
   ATX_MAP = {
     # 'ETA & LAMBDA 230V' => 'ATX',
-    'ATX V3.0 230V'     => 'ATX 3.0',
-    'ATX V3.1 230V'     => 'ATX 3.1',
+    'ATX V3.0 230V' => 'ATX 3.0',
+    'ATX V3.1 230V' => 'ATX 3.1',
   }
 
   def perform
@@ -30,7 +77,11 @@ class ReprocessPsusJob < ApplicationJob
       # add_for_manufaturer(temp_key)
 
       manufacturer_links.keys.each do |link|
-        add_for_manufaturer(link)
+        @manufacturer = @manufacturer_links[link]
+
+        unless not_wanted?(@manufacturer)
+          add_for_manufaturer(link)
+        end
       end
 
       PSUS.values.each do |data|
@@ -61,13 +112,11 @@ class ReprocessPsusJob < ApplicationJob
 
     ATX_MAP.each do |k, v|
       @logger.info "Processing #{v}"
-
       add_all_for(link, k, v)
     end
   end
 
   def add_all_for(link, button_text, atx_version)
-    manufacturer = @manufacturer_links[link]
     sleep 2
     button = @driver.find_element(link_text: button_text)
     button.click
@@ -80,10 +129,10 @@ class ReprocessPsusJob < ApplicationJob
 
     trs = @driver.find_elements(:css, '#myTable tr')[3..-1]
     trs.each do |tr|
-      data  = tr.find_elements(:tag_name, "td").map(&:text)[0..-3]
-      if viable?(data) && unbuyable?(manufacturer)
-        @logger.info "Got #{manufacturer} - #{data.first}"
-        attrs = data + [manufacturer, atx_version]
+      data = tr.find_elements(:tag_name, "td").map(&:text)[0..-3]
+      if viable?(data)
+        @logger.info "Got #{@manufacturer} - #{data.first}"
+        attrs = data + [@manufacturer, atx_version]
         PSUS.update(attrs.first => PSU.new(*attrs))
       end
     end
@@ -96,13 +145,7 @@ class ReprocessPsusJob < ApplicationJob
       data[2].to_i >= 1000
   end
 
-  def unbuyable?(manufacturer)
-    [
-      'LIAN LI',
-      'PCCOOLER',
-      'VETROO',
-      'MICRONICS',
-      'SHARKOON',
-    ].include?(manufacturer)
+  def not_wanted?(manufacturer)
+    SKIPPED_BRANDS.include?(manufacturer)
   end
 end
