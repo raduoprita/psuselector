5fd65dc1a8450c7ccb24539a6439bfe373ceebfe
Radu Oprita <oprita@gmail.com>
2024-07-26 22:21:04 +0200

    add precompiled assets

---
+++ public/assets/channels/index-c9355da2006c777cde15046eab8f06f98ad13c6bb6ed26d8605803ef24479d5a.js
@@ -0,0 +1,2 @@
+// Import all the channels to be used by Action Cable
+import "./psu_reprocess_channel";
---
+++ public/assets/controllers/toggler_controller-c60cc1173b108489cc695ffad7461d9f867cf54575d35032c2a16a343ccc081b.js
@@ -0,0 +1,8 @@
+import {Controller} from "@hotwired/stimulus"
+
+export default class extends Controller {
+    submit() {
+        let form = this.element.getElementsByTagName("form")[0];
+        form.submit();
+    }
+};
